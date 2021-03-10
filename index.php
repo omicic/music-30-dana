c1aef66e3edfbb3409e01191b367572ea0f1398e
Olivera Micic <omicic@gmail.com>
2021-03-10 15:07:24 +0100

    nav, footer

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-<?php include "partials/header.php";?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MusicApp</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+<body>
+    
+<nav>
+    <a href="index.php"><span>Music</span>App</a>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="how-it-work.php">How it works</a></li>
+        <li><a href="subscription.php">Subscription</a></li>
+    </ul>
+</nav>
+
+<footer>
+    <a href="index.php" class="logo"><span>Music</span>App</a>
+    <p>&copy; 2020. all right reserved</p>
+    <div class="icons">
+        <a href=""><i class="fab fa-facebook-square"></i></a>
+        <a href=""><i class="fab fa-instagram"></i></a>
+        <a href=""><i class="fab fa-soundcloud"></i></a>
+        <a href=""><i class="fab fa-youtube"></i></a>
+    </div>
+</footer>
+</body>
+</html>
