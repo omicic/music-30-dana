0dce0d9ff5c2d85b9b909058a27e892bb7243872
Olivera Micic <omicic@gmail.com>
2021-03-11 11:03:36 +0100

    howitworks

--- how-it-works.php
+++ how-it-works.php
@@ -1,7 +1,61 @@
 <?php include "partials/header.php"?>
 
 
-<h1 style="font-size:9rem; text-align:center;">How it works</h1>
+<section class="hivHeader">
+    <h2 class="container">How it works</h2>
+    <article class="container">
+        <div>
+            <i class="fa fa-user-circle"></i>
+            <h3>Create an account</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ducimus nesciunt tempore laboriosam
+                dolore? Illo odit, error quas, repudiandae necessitatibus voluptas molestias eligendi veritatis modi
+                sapiente, nesciunt quaerat. Ea, adipisci!</p>
+        </div>
+        <div>
+            <i class="fa fa-drum"></i>
+            <h3>Create an account</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ducimus nesciunt tempore laboriosam
+                dolore? Illo odit, error quas, repudiandae necessitatibus voluptas molestias eligendi veritatis modi
+                sapiente, nesciunt quaerat. Ea, adipisci!</p>
+        </div>
+        <div>
+            <i class="fa fa-cloud-download-alt"></i>
+            <h3>Create an account</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ducimus nesciunt tempore laboriosam
+                dolore? Illo odit, error quas, repudiandae necessitatibus voluptas molestias eligendi veritatis modi
+                sapiente, nesciunt quaerat. Ea, adipisci!</p>
+        </div>
+    </article>
+    <section class="container artists">
+
+        <article>
+            <h2>Concept & artists</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium velit ab esse tempora nisi maxime,
+                molestias minima commodi id voluptas odio, ipsam harum ex molestiae laudantium neque? Ad, hic eius?</p>
+        </article>
+
+        <article>
+            <div>
+                <img src="img/artists1.jpg" alt="">
+                <h3>Soul music</h3>
+            </div>
+            <div>
+                <img src="img/artists2.jpg" alt="">
+                <h3>Live concerts</h3>
+            </div>
+            <div>
+                <img src="img/artists3.jpg" alt="">
+                <h3>DJ set</h3>
+            </div>
+            <div>
+                <img src="img/artists4.jpg" alt="">
+                <h3>Live streams</h3>
+            </div>
+        </article>
+    </section>
+
+</section>
+
 
 
 <?php include "partials/footer.php"?>
--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php include "partials/header.php"?>
 
+<header class="mainHeader">
+    <div>
+        <h1><span>Music</span> for everyone</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi inventore a. Optio saepe expedita quasi
+            est a quisquam at.</p>
+        <button>Start free trial</button>
+    </div>
+    <div>
+        <img src="img/header.png" alt="">
+    </div>
+</header>
 
-<h1 style="font-size:9rem; text-align:center;">Home</h1>
+<section class="unlimited">
+    <article>
+        <h2>Unlimited Access to 100K</h2>
+    </article>
+    <article>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate natus fugit odit officiis ut soluta qui
+            autem, aperiam officia voluptatibus!</p>
+        <button>Try it now</button>
+    </article>
+</section>
 
 
 <?php include "partials/footer.php"?>
