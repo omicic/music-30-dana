0e779d47013a26198b3d9cba24a1462e8a1edb50
Olivera Micic <omicic@gmail.com>
2021-03-12 08:28:05 +0100

    subscription

--- subscription.php
+++ subscription.php
@@ -1,7 +1,54 @@
 <?php include "partials/header.php"?>
 
+<section class="subscription">
+    <article class="container">
+        <div>
+            <h2>Subscription from $15/month</h2>
+            <h3>Start a free trial now</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi inventore a. Optio saepe expedita
+                quasi
+                est a quisquam at.</p>
+            <button>Start free trial</button>
+        </div>
+        <div>
+            <ul>
+                <li>Play any track</li>
+                <li>Listen offline</li>
+                <li>No add interruptions</li>
+                <li>Unlimited skips</li>
+                <li>High quality</li>
+                <li>Shuffle play</li>
+            </ul>
 
-<h1 style="font-size:9rem; text-align:center;">Subscriptions</h1>
+        </div>
+    </article>
 
+</section>
 
+<section class="container concert">
+    <article>
+        <h2>Concept & artists</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium velit ab esse tempora nisi maxime,
+            molestias minima commodi id voluptas odio, ipsam harum ex molestiae laudantium neque? Ad, hic eius?</p>
+    </article>
+
+    <article>
+        <div>
+            <img src="img/subscription1.jpg" alt="">
+            <h3>No ad interruption</h3>
+        </div>
+        <div>
+            <img src="img/subscription2.jpg" alt="">
+            <h3>Hight quality</h3>
+        </div>
+        <div>
+            <img src="img/subscription3.jpg" alt="">
+            <h3>DJ set</h3>
+        </div>
+        <div>
+            <img src="img/subscription4.jpg" alt="">
+            <h3>Live streams</h3>
+        </div>
+    </article>
+</section>
 <?php include "partials/footer.php"?>
